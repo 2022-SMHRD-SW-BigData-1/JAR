aaaa7905e7ede9943844382a522f219eeee2c7f0
smhrd <smhrd@DESKTOP-6DGGVS8>
2022-08-31 11:25:13 +0900

    네번째 커밋

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -6,7 +6,8 @@ public class Ex01 {
 		System.out.println("첫번째 커밋");
 		// 두번째 커밋 출력문 >> 깃허브 사이트에 push
 		System.out.println("두번째 커밋");
-		
+		System.out.println("세번째 커밋");
+		System.out.println("네번째 커밋");
 	}
 
 }
